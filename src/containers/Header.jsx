6b66fbc9e2e5766e4a2e455fbb47bb562409dbae
vidit chawda <viditchawda301@gamil.com>
2023-04-09 22:11:44 +0530

    color

---
+++ src/containers/Header.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import styles from '../styles/containers/Header.module.scss'
+import { GiHamburgerMenu } from 'react-icons/gi';
+
+
+const Header = () => {
+
+  const [opennav, setopennav] = useState(false);
+
+  return (
+    <nav className={styles.mainnav} >
+      <div>© Designed By Vidit</div>
+      <div className={styles.navoptions}>
+        <a href="">Home</a>
+        <a href="">About Me</a>
+        <a href="">Skills</a>
+        <a href="">Experience</a>
+      </div>
+      {opennav && <div className={styles.navmenu}>
+        <a href="">Home</a>
+        <a href="">About Me</a>
+        <a href="">Skills</a>
+        <a href="">Experience</a>
+      </div>}
+      <div className={styles.hamburger}>
+        <GiHamburgerMenu onClick={() => {
+          setopennav(!opennav)
+        }} />
+      </div>
+    </nav>
+  )
+}
+
+export default Header
