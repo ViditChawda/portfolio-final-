8f925c5689a1d3a51159dfce188cc1aeeab298c2
vidit chawda <viditchawda301@gamil.com>
2023-04-11 22:31:42 +0530

    responsive navbar

---
+++ src/containers/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../styles/containers/Hero.module.scss'
+import bg from '../assets/sd.png'
+const Hero = () => {
+  return (
+    <div>
+      <img src='../assets/sd.png' alt="" />
+    </div>
+  )
+}
+
+export default Hero
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Header from '@/containers/Header'
+import Hero from '@/containers/Hero'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,6 +11,7 @@ export default function Home() {
   return (
     <>
     <Header/>
+    <Hero/> 
     </>
   )
 }
