e3b7010af32fbdafca948c0bdabd22482065f64c
vidit chawda <viditchawda301@gamil.com>
2023-04-14 01:32:42 +0530

    space in the content

--- src/containers/About.jsx
+++ src/containers/About.jsx
@@ -10,11 +10,11 @@ const About = () => {
       <Heading title="About" />
       <div className={styles.aboutWrapper}>
         <Image className={styles.aboutImage} src={myImg} alt="" />
-        <p className={styles.aboutContent}>Hi, <span>I'm Vidit Chawda</span>, a Computer Science Engineering student at <span>G H Raisoni College of Engineering</span>. I am passionate about technology and its potential to create positive change in the world. I have a strong interest in <span>mentoring and leadership</span>, and I believe in empowering others to achieve their goals.
+        <p className={styles.aboutContent}>Hi, <span>I'm Vidit Chawda</span>, a Computer Science Engineering student at <span>G H Raisoni College of Engineering</span>. I am passionate about technology and its potential to create positive change in the world. <br /> <br/> I have a strong interest in <span>mentoring and leadership</span>, and I believe in empowering others to achieve their goals.
 
-          As a <span>frontend developer</span>, I have a keen eye for design and aesthetics, and I enjoy creating beautiful and functional websites that provide an intuitive user experience. I am currently learning the <span>MERN stack</span>, and I am excited to apply my skills to build more complex web applications.
+          As a <span>frontend developer</span>. <br /> <br /> I am currently learning the <span>MERN stack</span>, and I am excited to apply my skills to build web applications. <br /> <br />
 
-          Apart from technology, I am also an artist and I love to express my creativity through various mediums like <span>painting, sketching, and digital art</span>. Art has always been an integral part of my life, and it helps me to think outside the box and bring a fresh perspective to my work.
+          Apart from technology, I am also an artist and I love to express my creativity through various mediums like <span>painting, sketching, and digital art</span>.
 
           Overall, I am a creative and motivated individual who enjoys tackling challenges and learning new things. I am always looking for opportunities to grow and develop my skills, and I believe that by working together, we can create a better world through technology.</p>
       </div>
