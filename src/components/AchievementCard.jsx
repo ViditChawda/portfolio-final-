4a33a40c143c391f95594fd3d0850824ad8769d5
viditchawda <viditchawda301@gmail.com>
2023-04-26 14:54:01 +0530

    achievements section

--- src/components/AchievementCard.jsx
+++ src/components/AchievementCard.jsx
@@ -5,14 +5,15 @@ import Image from 'next/image'
 function AchievementCard(props) {
   return (
     <div className={`${styles.achievementContainer} ${props.className}`}>
-      <div className={styles.sdkf}>
-        <Image></Image>
+      <div className={styles.imageWrapper}>
+        <Image className={styles.image} src={props.image}></Image>
       </div>
       <div className={styles.description}>
-        <p className={styles.title}>{}</p>
-        <p className={styles.date}>{}</p>
-        <p className={styles.body}>{}</p>
-        <p className={styles.icons}>{}</p>
+        <p className={styles.title}>{props.title}</p>
+        <p className={styles.subTitle}>{props.subTitle}</p>
+        <p className={styles.date}>{props.date}</p>
+        <p className={styles.body}>{props.body}</p>
+        <p className={styles.icons}>{props.icons}</p>
       </div>
     </div>
   )
--- src/containers/Achievements.jsx
+++ src/containers/Achievements.jsx
@@ -1,8 +1,10 @@
 import Heading from '@/components/Heading'
-import React from 'react'
+import React, {useRef} from 'react'
 import styles from '../styles/containers/Achievements.module.scss'
 import AchievementCard from '@/components/AchievementCard'
 import Slider from 'react-slick';
+import oops from '../assets/oops1.png'
+import sih from '../assets/sih1.png'
 
 function Achievements() {
   // const settings = {
@@ -12,18 +14,50 @@ function Achievements() {
   //   slidesToShow: 3,
   //   slidesToScroll: 1
   // };
+  const containerRef = useRef(null);
+
+  function scrollToSlide(slideNumber) {
+    const container = containerRef.current;
+    const slide = container.children[slideNumber];
+    slide.scrollIntoView({ behavior: "smooth" });
+  }
   return (
 
     <div className={styles.achContainer}>
       <Heading title="ACHIEVEMENT" />
-      <div className={styles.achWrapper}>
-        <AchievementCard  className={styles.carouselSlide} title="" />
-        <AchievementCard  className={styles.carouselSlide} title="" />
-        <AchievementCard  className={styles.carouselSlide} title="" />
-        <AchievementCard  className={styles.carouselSlide} title="" />
-        <AchievementCard  className={styles.carouselSlide} title="" />
-        <AchievementCard  className={styles.carouselSlide} title="" />
-        <AchievementCard  className={styles.carouselSlide} title="" />
+      <div className={styles.achWrapper} ref={containerRef}>
+        <AchievementCard className={styles.carouselSlide} image={sih} title="Joint Winner" subTitle="Smart India Hackthon" body="Hello everyone, Really happy to share that we stood 1st and won 1 lakh cash prize in smart India hackathon in one of the aicte problem statement, It overall was a great experience.
+
+I would really like to thank CHANDIGARH UNIVERSITY for great hospitality and
+for providing us such a great environment to work.
+"/>
+        <AchievementCard className={styles.carouselSlide} image={oops} title="Joint Winner" subTitle="Smart India Hackthon" body="Hello everyone, Really happy to share that we stood 1st and won 1 lakh cash prize in smart India hackathon in one of the aicte problem statement, It overall was a great experience.
+
+I would really like to thank CHANDIGARH UNIVERSITY for great hospitality and
+for providing us such a great environment to work.
+"/>
+        <AchievementCard className={styles.carouselSlide} image={sih} title="Joint Winner" subTitle="Smart India Hackthon" body="Hello everyone, Really happy to share that we stood 1st and won 1 lakh cash prize in smart India hackathon in one of the aicte problem statement, It overall was a great experience.
+
+I would really like to thank CHANDIGARH UNIVERSITY for great hospitality and
+for providing us such a great environment to work.
+"/>
+        <AchievementCard className={styles.carouselSlide} image={sih} title="Joint Winner" subTitle="Smart India Hackthon" body="Hello everyone, Really happy to share that we stood 1st and won 1 lakh cash prize in smart India hackathon in one of the aicte problem statement, It overall was a great experience.
+
+I would really like to thank CHANDIGARH UNIVERSITY for great hospitality and
+for providing us such a great environment to work.
+"/>
+        <AchievementCard className={styles.carouselSlide} image={sih} title="Joint Winner" subTitle="Smart India Hackthon" body="Hello everyone, Really happy to share that we stood 1st and won 1 lakh cash prize in smart India hackathon in one of the aicte problem statement, It overall was a great experience.
+
+I would really like to thank CHANDIGARH UNIVERSITY for great hospitality and
+for providing us such a great environment to work.
+"/>
+      </div>
+      <div className="carousel-nav">
+        <button onClick={() => scrollToSlide(0)}>Slide 1</button>
+        <button onClick={() => scrollToSlide(1)}>Slide 2</button>
+        <button onClick={() => scrollToSlide(2)}>Slide 3</button>
+        <button onClick={() => scrollToSlide(3)}>Slide 3</button>
+        <button onClick={() => scrollToSlide(4)}>Slide 3</button>
       </div>
     </div>
   )
