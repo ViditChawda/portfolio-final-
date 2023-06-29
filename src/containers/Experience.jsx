091c2747d38c4957b7131e62c806c95489373bf2
viditchawda <viditchawda301@gmail.com>
2023-06-29 12:27:49 +0530

    finall

--- src/containers/Experience.jsx
+++ src/containers/Experience.jsx
@@ -19,10 +19,10 @@ function Experience() {
             <div className={styles.expWrapper}>
                 <div data-aos="fade-top" className={styles.one}>
                     <Image className={styles.Image} src={bg} />
-                    <ExpCard title="> LEAD" subTitle="Google Developers Students Club" date="July 2022" body="I am the lead of the google developers students club at my college, i have good exeprience of managing and coducting the technical events " logo={gdsclogo} /> </div>
+                    <ExpCard title="> LEAD" subTitle="Google Developer Students Club" date="July 2022" body="I am the lead of the google developer students club at my college, i have good exeprience of managing and coducting the technical events " logo={gdsclogo} /> </div>
                 <div data-aos="fade-top" className={styles.two}>
-                    <ExpCard title="> ANDROID FACLITATOR" subTitle="Google Developers Students Club" date="Jan 2021" body="I facilitated the android styu jams in my college, where i conducted three live sessions on android app developemnt using Kotlin " logo={android} /> <Image className={styles.Image} src={and} /></div>
-                <div data-aos="fade-top" className={styles.one}> <Image className={styles.Image} src={exe} /> <ExpCard title="> EXECUTIVE COMMITTEE" subTitle="Google Developers Students Club" date="Aug 2021" body="Executive committee member of the google devvelopes student club, I have written a blog which was published by the GDSC GHRCE in medium" logo={gdsclogo} /> </div>
+                    <ExpCard title="> ANDROID FACLITATOR" subTitle="Google Developer Students Club" date="Jan 2021" body="I facilitated the android study jams in my college, where i conducted three live sessions on android app developemnt using Kotlin " logo={android} /> <Image className={styles.Image} src={and} /></div>
+                <div data-aos="fade-top" className={styles.one}> <Image className={styles.Image} src={exe} /> <ExpCard title="> EXECUTIVE COMMITTEE" subTitle="Google Developer Students Club" date="Aug 2021" body="Executive committee member of the google devvelopes student club, I have written a blog which was published by the GDSC GHRCE in medium" logo={gdsclogo} /> </div>
                 <div data-aos="fade-top" className={styles.two}><ExpCard title="> MERN STACK INTERN" subTitle="The Foodie Bee" date="Jun 2023" body="Working Mern Stack developer Intern designing UI and pages for the website based on cafe management system" logo={fb} /> <Image className={styles.Image} src={off} /> </div>
             </div>
         </div>
