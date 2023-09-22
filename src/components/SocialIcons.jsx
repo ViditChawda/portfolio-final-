aea530524c86d635a854369aa89d3237ab61f753
viditchawda <viditchawda301@gmail.com>
2023-09-22 12:49:17 +0530

    added a leetcode icon

--- src/components/SocialIcons.jsx
+++ src/components/SocialIcons.jsx
@@ -4,6 +4,7 @@ import { FiLinkedin } from 'react-icons/fi'
 import { FiInstagram } from 'react-icons/fi'
 import { FaXTwitter } from 'react-icons/fa6'
 import { BsMedium } from 'react-icons/bs'
+import { SiLeetcode } from 'react-icons/si'
 import styles from '../styles/components/SocialIcons.module.scss'
 
 const SocialIcons = () => {
@@ -17,6 +18,7 @@ const SocialIcons = () => {
                 <a href='https://twitter.com/ViditChawda723'><FaXTwitter className={styles.icon} /></a>
                 <a href='https://www.instagram.com/vidit_chawda.23/'><FiInstagram className={styles.icon} /></a>
                 <a href='https://www.linkedin.com/in/vidit-chawda-b4a740210/'><FiLinkedin className={styles.icon} /></a>
+                <a href='https://www.linkedin.com/in/vidit-chawda-b4a740210/'><SiLeetcode className={styles.icon} /></a>
                 <a><div className={styles.line}></div></a>
                 <div className={styles.line1}>
 
